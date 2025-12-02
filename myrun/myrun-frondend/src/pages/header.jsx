9beb91ba9c3abbdde013588a2257e924f9c983b7
손주언 <sje21@sookmyung.ac.kr>
2025-12-02 14:34:10 +0900

    토글 수정(미완)
    로그인 페이지에선 헤더가 보이지 않도록 수정 예정

--- myrun/myrun-frondend/src/pages/header.jsx
+++ myrun/myrun-frondend/src/pages/header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Header = () => {
     const [isOpen, setMenu] = useState(false);
@@ -6,8 +6,12 @@ const Header = () => {
     const toggleMenu = () => {
         setMenu(!isOpen);
     }
+    const closeMenu=() => {
+        setMenu (false);
+    }
 
     return (
+        <>
         <header className="main-header">
             <div className="main-header-logo">MyRun</div>
             <button className="main-header-menu" onClick={toggleMenu}>
@@ -15,8 +19,22 @@ const Header = () => {
                 <span />
                 <span />
             </button>
-
         </header>
+            {isOpen &&(
+                <nav className="header-menu-open">
+                        <ul style={{ listStyleType: "none" }}> 
+                            {/*메뉴 아이템, 버튼 누르면 메뉴 닫힘*/}
+                            <li><a href="/main" className="menu-list" >홈</a></li>
+                            <li><a href="/record" className="menu-list">기록하기</a></li>
+                            <li><a href="/recommend" className="menu-list">코스추천</a></li>
+                            <li><a href="/mypage" className="menu-list">마이페이지</a></li>
+                            <li><a href="/" className="menu-list">로그아웃</a></li>
+                        </ul>
+                    </nav>
+            )
+        }
+        </>
     );
+    
 }
 export default Header;
